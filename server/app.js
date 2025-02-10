2b0d2db18c387174da9199ef36b692b0e7f74098
Marta Prystatska <tradlisboa@gmail.com>
2025-02-10 18:23:42 +0000

    create message.contraller +(message.requests) BUT WE CREATE E USED FILE:post.mongod ---> post info direcly to messages in database chat_dev(watch lesson Websocket_128_1 34min)

--- server/app.js
+++ server/app.js
@@ -7,8 +7,16 @@ const {
     updateUser,
     deleteUser,
     deleteManyUsers,
-    getAllUsers } = require('./controllers/user.controller');
+    getAllUsers
+} = require('./controllers/user.controller');
 
+const {
+    createNewMessage,
+    getMessage,
+    getMessages,
+    updateMessage,
+    deleteMessage
+} = require('./controllers/message.controller'); 
 
 
 
@@ -16,9 +24,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+//app.get('/', (req, res) => {res.send('Hello World!');});
+app.get('/', getMessages); 
 
 
 app.post('/users', createOrFindUser);
@@ -27,6 +34,12 @@ app.get('/users/:userId', getUserById);
 app.patch('/users/:userId', updateUser);
 app.delete('/users/:userId', deleteUser);
 app.delete('/users', deleteManyUsers);
+
+
+app.post('/users/:userId/messages', createNewMessage); 
+app.get('/users/:userId/messages/:messageId', getMessage); 
+app.patch('/users/:userId/messages/:messageId', updateMessage);  
+app.delete('/users/:userId/messages/:messageId', deleteMessage);
 
 
 
---
+++ server/controllers/message.controller.js
@@ -0,0 +1,141 @@
+const { User } = require('../models');
+const { Message } = require('../models');
+
+
+module.exports.createNewMessage = async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const messageData = req.body;
+
+        // Створюємо нове повідомлення
+        const newMessage = await Message.create({ ...messageData, userId });
+        
+        if (!newMessage) {
+            return res.status(400).send({ errors: [{ detail: 'Message not created' }] });
+        }
+
+        res.status(201).send({ data: newMessage });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+module.exports.getMessagesByUser = async (req, res, next) => {
+    try {
+        const { params: { userId } } = req;
+        const messages = await Message.find({ userId }).sort({ createdAt: -1 });
+
+        if (!messages.length) {
+            return res.status(404).send({ errors: [{ detail: 'No messages found' }] });
+        }
+
+        res.status(200).send({ data: messages });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.getChatMessages = async (req, res, next) => {
+    try {
+        const { params: { userId1, userId2 } } = req;
+        const messages = await Message.find({
+            $or: [
+                { userId: userId1, receiverId: userId2 },
+                { userId: userId2, receiverId: userId1 }
+            ]
+        }).sort({ createdAt: 1 });
+
+        if (!messages.length) {
+            return res.status(404).send({ errors: [{ detail: 'No chat messages found' }] });
+        }
+
+        res.status(200).send({ data: messages });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.deleteMessage = async (req, res, next) => {
+    try {
+        const { params: { messageId } } = req;
+        const deletedMessage = await Message.findByIdAndDelete(messageId);
+
+        if (!deletedMessage) {
+            return res.status(404).send({ errors: [{ detail: 'Message not found' }] });
+        }
+
+        res.status(200).send({ data: deletedMessage });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+
+
+
+
+
+
+module.exports.getMessage = async (req, res, next) => {
+    try {
+        const { params: { messageId } } = req;
+        if (!messageId) {
+            return res.status(400).send({ errors: [{ detail: 'Message ID is required' }] });
+        }
+        const message = await Message.findById(messageId);
+        if (!message) {
+            return res.status(404).send({ errors: [{ detail: 'Message not found' }] });
+        }
+        res.status(200).send({ data: message });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.getUserMessages = async (req, res, next) => {
+    try {
+        const { params: { userId } } = req; //only query params,not existing body
+        if (!userId) {
+            return res.status(400).send({ errors: [{ detail: 'User ID is required' }] });
+        }
+        const user = await User.findById(userId).populate('messages');
+        if (!user) {
+            return res.status(404).send({ errors: [{ detail: 'User not found' }] });
+        }
+        res.status(200).send({ data: user.messages });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.getMessages = async (req, res, next) => {
+    try {
+        //only query params,not existing body
+        const messages = await Message.find().populate({
+            path: 'userId',
+            select: 'login'
+        });
+        if (!messages) {
+            return next(new Error('Messages not found'));
+        }
+        res.status(200).send({ data: messages });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.updateMessage = async (req, res, next) => {
+    try {
+        const { params: { messageId }, body } = req;
+        const updatedMessage = await Message.findByIdAndUpdate(messageId, body, { new: true });
+        if (!updatedMessage) {
+            return res.status(404).send({ errors: [{ detail: 'Message not found' }] });
+        }
+        res.status(200).send({ data: updatedMessage });
+    } catch (error) {
+        next(error);
+    }
+};  
