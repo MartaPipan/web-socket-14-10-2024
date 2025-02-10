97daded45e71a189698a6ba91e82dbcd2df2b95a
Marta Prystatska <tradlisboa@gmail.com>
2025-02-10 18:43:13 +0000

    !!! add comment about create file with  MongoDB

--- server/app.js
+++ server/app.js
@@ -24,8 +24,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//app.get('/', (req, res) => {res.send('Hello World!');});
-app.get('/', getMessages); 
+
+app.get('/', getMessages); //we used mongoDB docs for cteste bd and file in server---> post.mongodb 
 
 
 app.post('/users', createOrFindUser);
